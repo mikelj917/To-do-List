256e3043c0f55f4756bfefb2bd68ea63f771c00c
Vinicius <viniciusaugustooliveira2007@gmail.com>
2025-07-01 11:38:16 -0300

    feat: improve filters button behavior and support adding tasks to array

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,18 @@
-const filterBtnStates = {
-  idle: "filter-btn cursor-pointer rounded-md bg-zinc-200 px-4 py-1 text-sm font-medium text-zinc-800 transition-colors hover:bg-zinc-300",
-  active: "filter-btn cursor-pointer rounded-md bg-green-500 px-4 py-1 text-sm font-medium text-white transition-colors",
-}
-const filterBtns = document.querySelectorAll(".filter-btn");
+let tasks = [];
 
-filterBtns.forEach((btn) => {
-  btn.addEventListener("click", function activate() {
-    filterBtns.forEach((btn) => {
-      btn.dataset.active = false;
-      btn.className = filterBtnStates.idle;
-    });
+// Add tasks to the array(tasks)
+document.getElementById("addTask-btn").addEventListener("click", () => {
+  const taskInput = document.getElementById("task-input");
+  taskText = taskInput.value.trim();
 
-    btn.dataset.active = true;
-    btn.className = filterBtnStates.active;
-  });
-})
+  if(taskText === "") {
+    return alert("Digite uma tarefa...")
+  }
+
+  tasks.push({
+    task: taskText,
+    completed: false
+  })
+  
+  taskInput.value = "";
+});
