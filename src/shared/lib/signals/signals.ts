ba1f58e5b6eb9eb6ef13876ba2e5fdfa86235161
Vinicius <viniciusaugustooliveira2007@gmail.com>
2025-08-29 11:53:52 -0300

    chore: create base signals library

---
+++ src/shared/lib/signals/signals.ts
@@ -0,0 +1,43 @@
+let activeEffect: (() => void) | null = null;
+const effectQueue = new Set<() => void>();
+let isFlushing: boolean = false;
+
+function scheduleRun(fn: () => void) {
+  effectQueue.add(fn);
+  if (!isFlushing) {
+    isFlushing = true;
+    Promise.resolve().then(() => {
+      try {
+        Array.from(effectQueue).forEach(effect => effect())
+      } finally {
+        effectQueue.clear();
+        isFlushing = false;
+      }
+    });
+  }
+}
+
+export function signals<T>(initialValue: T) {
+  let value: T = initialValue;
+  const subscribers = new Set<() => void>();
+
+  return {
+    get() {
+      if (activeEffect) subscribers.add(activeEffect);
+      return value;
+    },
+    set(newValue: any) {
+      value = newValue
+      Array.from(subscribers).forEach(sub => scheduleRun(sub))
+    }
+  };
+}
+
+export function effect(fn: () => void) {
+  const wrapped = () => {
+    activeEffect = wrapped
+    fn()
+    activeEffect = null;
+  }
+  wrapped()
+}
